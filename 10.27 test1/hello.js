be5e9079bcb0530454170a5137930f142dd22cf5
6worry <dudfhr217@naver.com>
2023-10-27 13:36:48 +0900

    1차 커밋

---
+++ 10.27 test1/hello.js
@@ -0,0 +1,20 @@
+console.log('hello node');
+
+a = 5;
+console.log(a);
+
+a = "hi";
+console.log(a);
+
+var a =6;
+console.log(a);
+
+let b =7;
+console.log(b);
+
+let name = "6worry";
+let age = 23;
+
+const pi = 3.141592
+
+console.log(name, age, pi);
