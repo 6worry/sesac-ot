46f3726cfdb6ef259bd6b424b841e12a23db0240
6worry <dudfhr217@naver.com>
2023-10-30 04:37:01 +0900

    최종 커밋

---
+++ 10-26 숙제/10.26 hw.js
@@ -0,0 +1,58 @@
+let lastClickedButton = null;
+
+    function addnumber (value) {
+        document.getElementById('결과값').innerText += value;
+        console.log(value);
+    }
+    
+    function number_inc() {
+        let currentResult = document.getElementById('결과값').innerText;
+        console.log("+");
+        if (currentResult.length === 0 || (currentResult.charAt(currentResult.length - 1) !== '+' && currentResult.charAt(currentResult.length - 1) !== '-' && currentResult.charAt(currentResult.length - 1) !== '*' && currentResult.charAt(currentResult.length - 1) !== '/')) {
+            document.getElementById('결과값').innerText += '+';
+        }
+    }
+    
+    function number_dec() {
+        let currentResult = document.getElementById('결과값').innerText;
+        console.log("-");
+        if (currentResult.length === 0 || (currentResult.charAt(currentResult.length - 1) !== '+' && currentResult.charAt(currentResult.length - 1) !== '-' && currentResult.charAt(currentResult.length - 1) !== '*' && currentResult.charAt(currentResult.length - 1) !== '/')) {
+            document.getElementById('결과값').innerText += '-';
+        }
+    }
+    
+    function number_mul() {
+        let currentResult = document.getElementById('결과값').innerText;
+        console.log("*");
+        if (currentResult.length === 0 || (currentResult.charAt(currentResult.length - 1) !== '+' && currentResult.charAt(currentResult.length - 1) !== '-' && currentResult.charAt(currentResult.length - 1) !== '*' && currentResult.charAt(currentResult.length - 1) !== '/')) {
+            document.getElementById('결과값').innerText += '*';
+        }
+    }
+    
+    function number_div() {
+        let currentResult = document.getElementById('결과값').innerText;
+        console.log("/");
+        if (currentResult.length === 0 || (currentResult.charAt(currentResult.length - 1) !== '+' && currentResult.charAt(currentResult.length - 1) !== '-' && currentResult.charAt(currentResult.length - 1) !== '*' && currentResult.charAt(currentResult.length - 1) !== '/')) {
+            document.getElementById('결과값').innerText += '/';
+        }
+    }
+    
+    function equlelement() {
+        let result = document.getElementById('결과값').innerText;
+        let calculatedResult = eval(result);
+        console.log("=");
+        if (calculatedResult === undefined) {
+                document.getElementById('결과값').innerText = "";
+        } else if (calculatedResult === calculatedResult) {
+                document.getElementById('결과값').innerText = calculatedResult;
+                console.log(calculatedResult);
+        } else {
+                document.getElementById('결과값').innerText = calculatedResult;
+                console.log(result + '=' + calculatedResult);
+        }
+    }
+
+    function removeelement(){
+        document.getElementById('결과값').innerText = "";
+        console.log("C")
+    }
