948f010c98ddee76cd7f2154364cf57cc45862db
6worry <dudfhr217@naver.com>
2023-11-01 08:29:16 +0900

    1차 커밋

--- 10.31 숙제/함수.js
+++ 10.31 숙제/함수.js
@@ -6,3 +6,16 @@
 // const res = text2.indexOf(' ');
 // console.log(res);
 
+const number = [11, 12, 13, 14, 15];
+
+function max_number(num){
+    let num1 = num[0];
+    for (let i=0; i<num.length; i++){
+        if(num[i] > num1){
+            num1 = num[i]
+            console.log(num1);
+        }
+    } return num1;
+}
+max_num1 = max_number(number);
+console.log(max_num1);
