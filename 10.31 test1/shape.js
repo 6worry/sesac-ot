396a446b7260f6a4cada1c15e4cc44eef749c250
6worry <dudfhr217@naver.com>
2023-11-02 07:23:32 +0900

    아침공부

--- 10.31 test1/shape.js
+++ 10.31 test1/shape.js
@@ -1,4 +1,8 @@
 class Shape{
+    constructor(type){
+        this.type = type;
+    }
+
     getArea() {
         return 0;
     }
@@ -27,7 +31,6 @@ class Square extends Shape{
     getInfo(){
         return `정사각형, 변수의 길이= ${this.sideLength}`
     }
-
 }
 
 class Triangle extends Shape{
@@ -36,8 +39,7 @@ class Triangle extends Shape{
         this.width = width;
         this.height = height;
     }
-    
-    
+
     getArea(){
         return this.width * this.height /2;
     }
@@ -45,40 +47,54 @@ class Triangle extends Shape{
     getInfo(){
         return `삼각형, 밑변의 길이= ${this.width}, 높이의 길이= ${this.height}`
     }
-
 }
 
 
 class Circle extends Shape{
     constructor(radius){
         super();
-        this.radius = this.radius;
+        this.radius = radius;
     }
 
     getArea(){
         return Math.PI *this.radius **2;
     }
-}
 
-class Trapezium extends Shape{
-    constructor(){
-
+    getInfo(){
+        return `원, 반지름의 길이=${this.radius}`
     }
 }
 
+class Trapezium extends Shape{
+    constructor(upperwidth, lowerwidth, height){
+        super();
+        this.upperwidth = upperwidth;
+        this.lowerwidth = lowerwidth;
+        this.height = height;
+    }
+
+    getArea(){
+        return this.upperwidth * this.lowerwidth * this.height /2;
+    }
+
+    getInfo(){
+        return `사다리꼴, 윗변의 길이=${this.upperwidth}, 밑변의 길이=${this.lowerwidth}, 높이=${this.height}`
+    }    
+}
+
 const square = new Square(5);
-const triangle = new Triangle(5);
-const circle = new Circle(5);
-const trapezium = new Trapezium(5);
+const triangle = new Triangle(10, 8);
+const circle = new Circle(7);
+const trapezium = new Trapezium(9, 11, 15);
 console.log('Square Area:', square.getArea());
 console.log('Triangle Area:', triangle.getArea());
 console.log('Circle Area:', circle.getArea());
 console.log('Trapezium Area:', trapezium.getArea());
-console.log('Square Info:', square.getarea());
-console.log('Triangle Info:', triangle.getarea());
-console.log('Circle Info:', circle.getarea());
-console.log('Square Info:', Square.getarea());
-console.log('Square Info:', square.getarea());
-console.log('Triangle Info:', triangle.getarea());
-console.log('Circle Info:', circle.getarea());
-console.log('Square Info:', Square.getarea());
+console.log('Square Info:', square.getInfo());
+console.log('Triangle Info:', triangle.getInfo());
+console.log('Circle Info:', circle.getInfo());
+console.log('Trapezium Info:', trapezium.getInfo());
+console.log('Square:', square.toString());
+console.log('Triangle:', triangle.toString());
+console.log('Circle:', circle.toString());
+console.log('Trapezium:', trapezium.toString());
