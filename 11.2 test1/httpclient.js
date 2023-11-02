cb08225016a521dbad1d769b1c1a420da92a98ab
6worry <dudfhr217@naver.com>
2023-11-02 11:52:30 +0900

    1차 커밋

---
+++ 11.2 test1/httpclient.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+//요청하고 싶은 주소 정의
+
+const options ={
+    hostname: 'www.naver.com',
+    port: 80,
+    path: '/',
+    method: 'GET'
+};
+
+const req = http.request(options, (res) => {
+    console.log(`상태코드: ${res.statusCode}`);
+    res.on('data', (chunk) =>{
+        console.log(`데이터수신: ${chunk}`);
+    })
+});
+
+req.on('error', (error) =>{
+    console.error(`요청중 오류 발생, ${error}`);
+})
+
+req.end();
+
---
+++ 11.2 test1/url.js
@@ -0,0 +1,25 @@
+const url = require('url');
+
+const myUrl = 'https://chat.openai.com/c/963af72d-1f34-443a-916c-3c6e062ab014';
+
+// URL Pasing
+
+const parsedUrl = url.parse(myUrl, true);
+console.log('파싱된 URL:', parsedUrl);
+console.log('호스트:', parsedUrl.host);
+console.log('경로:', parsedUrl.pathname);
+console.log('쿼리:', parsedUrl.query);
+
+const myUrl2 = {
+    protocol: 'https',
+    hostname: 'www.naver.com',
+    pathname: '/search.naver',
+    query: {
+        query:'sesac'
+    }
+};
+
+// URL 조립
+
+const assembleUrl = url.format(myUrl2);
+console.log('조립된 URL:', assembleUrl);
