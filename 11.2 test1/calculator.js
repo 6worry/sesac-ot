b353aa66a72630124fa30beeac389754e589e276
6worry <dudfhr217@naver.com>
2023-11-02 15:00:18 +0900

    1차 커밋

---
+++ 11.2 test1/calculator.js
@@ -0,0 +1,18 @@
+export function add(a, b){
+    return a+b;
+}
+
+export function sub(a, b){
+    return a-b;
+}
+
+export function mul(a, b){
+    return a*b;
+}
+
+export function div(a, b){
+    return a/b;
+}
+
+// module.exports = node.js 초기방식
+//ES6버전부터 import/export 기능 생김
---
+++ 11.2 test1/datagen.js
@@ -0,0 +1,31 @@
+// 랜덤으로 100개의 숫자 만들기
+
+const array = Array.from({length:100}, ()=> Math.floor(Math.random()*100));
+
+console.log(array);
+
+//랜덤으로 100개의 숫자 겹치지 않게 만들기
+
+const uniqueRandomNumbers = new Set();
+console.log(uniqueRandomNumbers.size);
+
+while(uniqueRandomNumbers.size <100){
+    uniqueRandomNumbers.add(Math.floor(Math.random()*100));
+}
+
+console.log(uniqueRandomNumbers);
+
+const array2 = Array.from(uniqueRandomNumbers);
+
+console.log(array2);
+
+//직접 구현할 시
+function uniqueRandium() {
+let uniqueRandomNumbers2 = [];
+while(uniqueRandomNumbers2.length <100){
+    let randomNumber = Math.floor(Math.random()*100);
+    for (let i=0; i<uniqueRandomNumbers2.length;i++){
+        
+    }
+    }
+}
