5947be58bff8921addd91165c38abcd875b6df3e
6worry <dudfhr217@naver.com>
2023-11-15 14:00:03 +0900

    1차커밋

--- 11.14 test1/app5.js
+++ 11.14 test1/app5.js
@@ -1,10 +1,17 @@
 const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
+// const querystring = require ('querystring');
+// = const parse = querystring.parse;
+// =
+//객체 디스트럭처링 (de-structuring)
+const {parse} = require('querystring')
 
 const Success = 200;
 const Server_Error = 500;
 const Not_Found = 404;
+
+const users={};
 
 const server = http.createServer(async (req, res) => {
     console.log(req.method, req.url);
@@ -29,7 +36,10 @@ const server = http.createServer(async (req, res) => {
                 const data = await fs.readFile('./about.html');
                 res.writeHead(Success, {'Content-Type': 'text/html; charset=utf-8'});
                 res.end(data);
-            } 
+            } else if (req.url ==='/user') {
+                res.writeHead(Success, {'Content-Type': 'text/plain; charset=utf-8'});
+                res.end(JSON.stringify(users)); // JSON 파일을 문자열로 바꿔줌
+            }
             // else if(req.url === '/photo3.jpg') {
             //     const data = await fs.readFile('images/photo3.jpg');
             //     res.writeHead(Success, {'Content-Type': 'image/jpg'});
@@ -55,16 +65,75 @@ const server = http.createServer(async (req, res) => {
                 res.end('Nothing!');
             };
         } else if(req.method === 'POST') {
-            //요청을 생성할 때 사용
-            res.writeHead(201);
-            res.end('등록 성공');
+            if(req.url ==='/user'){
+
+                //요청을 생성할 때 사용
+                //요청 request를 파싱해서 처리함
+                //데이터 전송 명령어: curl -X POST 127.0.0.1:3000/user -H "Content-Type: multipart/form-data" -d "name=aaa"
+            
+                let body='';
+                req.on('data', (data)=>{
+                body += data
+                });
+                req.on('end', ()=>{
+                console.log('요청내용:', body);
+                const formData = parse(body)
+                console.log('파싱 이후:', formData);
+                
+                const username = formData.name;
+                console.log('사용자 이름:', username);
+                
+                users[username] = username;
+                console.log('최종객체:', users)
+            });
+            
+            //결과 response를 주는 코드
+                res.writeHead(201, {'Content-Type': 'text/plain; charset=utf-8'});
+                res.end('등록 성공');
+            }
         } else if (req.method === 'PUT') {
+
             //요청을 수정할 때 사용
+            //수정 명령어 : curl -X PUT 127.0.0.1:7600/user/aaa -d "name=bbb"
+
+            res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
             res.end('수정 성공');
         }else if(req.method === 'DELETE') {
+
             //요청을 삭제할 때 사용
-            res.end('삭제 성공');
-        };
+            //요청에 대한 파싱
+            //삭제 명령어 : curl -X DELETE 127.0.0.1:7600/user/aaa
+            //1. url에 /users/ 시작하는걸 찾고
+            //2. 그 뒤에 있는 글자를 읽어서 key 처리
+            //3. 그 키를 users라는 객체안에서 삭제
+            if(req.url.startsWith('/user')){
+                let body='';
+                req.on('data', (data)=>{
+                body += data
+                });
+                req.on('end', ()=>{
+                    console.log('요청내용:', body);
+                    const formData = parse(body)
+                    console.log('파싱 이후:', formData);
+                    
+                    const username = req.url.split('/').pop();
+                    console.log('삭제할 내용:', username);
+                    users[username] = username;
+                    delete users[username];
+                    console.log('최종객체:', users)
+                });
+                
+                
+                //요청을 생성할 때 사용
+                //요청 request를 파싱해서 처리함
+                
+                
+                //요청에 대한 응답결과
+                res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+                res.end('삭제 성공');
+            }
+            
+        }
     } catch(err) {
         console.error('오류!', err.message);
         res.writeHead(Server_Error, {'Content-Type': 'text/plain; charset=utf-8'});
