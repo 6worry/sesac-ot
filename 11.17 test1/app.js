c6bd72433068d36693ec847fa4512ad6c022791e
6worry <dudfhr217@naver.com>
2023-11-17 10:36:46 +0900

    1차 커밋

--- 11.17 test1/app.js
+++ 11.17 test1/app.js
@@ -2,3 +2,23 @@ const express = require('express');
 
 const app = express();
 
+const port = 3001;
+
+function myLogger(req, res, next) { 
+    const date = new Date(Date.now());
+    const formattedTime = date.toLocaleString();
+    console.log(`${formattedTime}: log message`);
+    next();
+}
+
+// 1. 라우팅
+app.get('/', (req, res) => {
+    res.send('Hi');
+}); // 루트 경로 설정
+
+// 2. 미들웨어
+app.use(myLogger);
+
+app.listen(port, () => {
+    console.log(`서버 ${port}번 포트 실행중`);
+});
