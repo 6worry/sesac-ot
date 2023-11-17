764d4f1daa52b0b873be9b09ef74f0b19e0b9b1f
6worry <dudfhr217@naver.com>
2023-11-17 17:34:57 +0900

    집가서 try 오류 수정하기

--- 11.17 test1/test.js
+++ 11.17 test1/test.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3001;
 const fs = require('fs');
 const path = require('path');
-
+const {parse} = require('querystring');
+const users ={};
 // app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     const htmlFilePath = path.join(__dirname, 'public', 'new index.html');
@@ -34,6 +36,34 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/user', (req, res) => {
+    // const jsFilePath = path.join(__dirname, 'public', 'user.js');
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.end(JSON.stringify(users));
+});
+
+app.post('/user', (req, res) => {
+    let body='';
+    req.on('data', (data)=>{
+    body += data
+    });
+    req.on('end', ()=>{
+        try{
+            // const formData = JSON.parse(body)
+                
+            // const username = formData.name;
+            
+            // const id = Date.now();
+            
+            // users[id] = username;
+            // res.json({receiveData: formData})
+        } catch (err) {
+            res.status(400).json({err: "입력값이 잘못됐다."});
+        };
+    });
+    
+    //결과 response를 주는 코드
+        res.writeHead(201, {'Content-Type': 'text/plain; charset=utf-8'});
+        res.end('등록 성공');
     
 });
 
