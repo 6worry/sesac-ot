a85f37d5918570d292c4048233587655bc6440a1
6worry <dudfhr217@naver.com>
2023-11-20 10:37:46 +0900

    1차 커밋

--- 11.20 test1/app.js
+++ 11.20 test1/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3002;
+const users = {};
 
-app.use('/public', express.static('public'));
-app.use('/public', express.static('images'));
+app.use('/static', express.static('public/static'));
+app.use('/images', express.static('public/static'));
 
 app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'new about.html'));
+});
+
+app.get('/user', (req, res) => {
+    res.json(users);
 });
 
 app.listen(port, () => {
