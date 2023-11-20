91072f454064006af2dfe1b2f17cdfd05749aba6
6worry <dudfhr217@naver.com>
2023-11-20 11:43:38 +0900

    1차 커밋

--- 11.20 test1/app2.js
+++ 11.20 test1/app2.js
@@ -5,6 +5,16 @@ const port = 3002;
 
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => { // 템플릿 엔진을 통해 렌더링 필요
+    res.render('index', {title: 'THIS IS EXPRESS', message: '계획대로 되고 있어'});
+});
+
+app.get('/greeting', (req, res) => {
+    const username = '진영록';
+
+    res.render('greeting', {username: username})
+});
+
 app.listen(port, () => {
     console.log(`${port}번 실행 완료`);
 });
