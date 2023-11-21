f791037756c6c9f52474c81af23038e2c9356cd2
6worry <dudfhr217@naver.com>
2023-11-21 11:44:18 +0900

    1차 커밋

--- 11.20 test1/11.20hw.js
+++ 11.20 test1/11.20hw.js
@@ -7,17 +7,31 @@ const app = express();
 const port = 3003;
 
 nunjucks.configure('views hw', {
-    express: app
+    express: app,
+    autoescape: true
 });
 
 app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
     const data = []; // 읽은 데이터를 담을 곳
-    fs.readFileSync('user.csv', {encoding: 'utf-8'})
-   
+    const header = [];
+    fs.createReadStream('data.csv', {encoding: 'utf-8'})
+    .pipe(csv())
+    .on('headers', (headers) => {
+        header.push(...headers)
+        console.log(header)
+    })
+    .on('data', (row) => {
+        data.push(row);
+    })
+    .on('end', () => {
+        res.render('index', {data: data, headers: header});
+    })
+    .on('error', (err) => {
+        
+    })
     // === 파일 읽기
-    res.render('index', { data });
 });
 
 app.listen(port, () => {
