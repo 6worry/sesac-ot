82ff7d1b2c7612e061bb406de89ae7c09a9026d7
6worry <dudfhr217@naver.com>
2023-11-22 11:10:48 +0900

    1차 커밋

---
+++ 11.22 test1/app2.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cookieParser =require('cookie-parser');
+
+const app = express();
+const port = 3004;
+
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.cookie('mycookie', 'test3');
+    res.send('쿠키 생성 완료');
+});
+
+app.get('/readcookie', (req, res) => {
+    const myCookie = req.cookies.mycookie
+    console.log(myCookie);
+
+    res.send(`내 쿠키 = ${myCookie}`)
+});
+
+app.listen(port, () => {
+    console.log(`${port} 실행 완료`);
+});
