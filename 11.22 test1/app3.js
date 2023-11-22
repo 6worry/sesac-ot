4fd93a5b878385720a2de6c2ac3d4f4119780913
6worry <dudfhr217@naver.com>
2023-11-22 13:57:54 +0900

    1차 커밋

---
+++ 11.22 test1/app3.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+const port = 3004;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.use(session({
+    secret: 'my-key2',
+    resave: false,
+    saveUninitialized: true
+}));
+
+const users = [
+    {id: 1, userid: 'user1', userpw: 'pw1'},
+    {id: 2, userid: 'user2', userpw: 'pw2'}
+];
+
+app.get('/', (req, res) => {
+    res.send('Login 하시오');
+});
+
+app.get('/profile', (req, res) => {
+    const user = req.session.user
+
+    if (user) {
+        res.json({userid: user.userid, message: '프로필 정보'})
+    } else {
+        res.status(401).json({message: '로그인을 하라고'});
+    };
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log('삭제오류', err);
+            res.status(500).json({message: '실패!'});
+        } else {        
+            console.log('로그아웃 성공');
+            res.json({message: 'logout success'});
+        };
+    });
+});
+
+app.post('/login', (req, res) => {
+    const { userid, userpw } = req.body;
+    console.log(userid, userpw);
+
+    const user = users.find((u) => 
+        u.userid === userid
+        && u.userpw === userpw
+    );
+
+    if (user) {
+        console.log('로그인 성공');
+        req.session.user = user
+        res.json({message: 'login success'});
+    } else {
+        console.log('로그인 실패');
+        res.status(401).json({message: 'login fail'});
+    };
+});
+
+
+app.listen(port, () => {
+    console.log(`${port}번 생성 완료`);
+});
--- 11.22 test1/session.js
+++ 11.22 test1/session.js
@@ -10,9 +10,17 @@ app.use(session({
     saveUninitialized: true // 초기화 되지 않은 세션을 저장소에 저장할지에 대한 여부 확인
 }));
 
+app.use((req, res, next) => {
+    req.session.visitCount = req.session.visitCount || 0; // 변수인 visitCount 값 초기화
+    req.session.visitCount++; // 방문 횟수 증가
+
+    console.log('SessionID:', req.sessionID);
+    console.log('SessionInfo:', req.session);
+    next();
+});
+
 app.get('/', (req,res) => {
-    console.log(req.session);
-    res.send('GOOD');
+    res.send(`너의 방문 횟수: ${req.session.visitCount}`);
 });
 
 app.listen(port, () => {
