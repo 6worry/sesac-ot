3fa52b42fd48093b1dabb95b44a6cbf7b9b6c0d1
6worry <dudfhr217@naver.com>
2023-11-23 10:49:08 +0900

    1차 커밋

---
+++ 11.23 test1/public/js/cart.js
@@ -0,0 +1,8 @@
+function product() {
+    const productname = document.getElementById('product1').value
+    const productprice = document.getElementById('product1').value
+
+    fetch('/products', {
+        
+    })
+}
