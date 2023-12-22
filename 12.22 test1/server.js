a39406de857b124e23c0a67e8494d0db6d002c9f
6worry <dudfhr217@naver.com>
2023-12-22 11:08:38 +0900

    1차 커밋

--- 12.22 test1/server.js
+++ 12.22 test1/server.js
@@ -12,12 +12,12 @@ wss.on('connection', (ws, req) => {
     const clientIp = req.socket.remoteAddress;
     console.log('클라이언트 접속함', clientIp); // 소켓 연결 요청온 것에 대하여 처리함
         
-    wss.on('message', () => {
-        // 연결된 이후 내부 메세지 처리하는 부분 
+    ws.on('message', (message) => {
+        console.log(message); // 연결된 이후 내부 메세지 처리하는 부분 
     });
         
         
-    wss.on('close', () => {
+    ws.on('close', () => {
         // 연결된 이후 연결 종료를 처리하는 부분
     });
 });
