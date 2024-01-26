31c0a1c864f70d4cca7bdd7145a258315961b244
6worry <dudfhr217@naver.com>
2024-01-26 18:24:04 +0900

    최종커밋

--- 01.26 test1/lyric.js
+++ 01.26 test1/lyric.js
@@ -1,21 +1,54 @@
 const axios = require('axios');
-
+const cheerio = require('cheerio');
 const base_url = 'https://m.search.naver.com/p/csearch/content/qapirender.nhn';
 
 const searchLyric = async(query) => {
-    const response = await axios.get(base_url, {
-        params: {
-            where: "nexearch",
-            key: "LyricsSearchResult",
-            pkid: "519",
-            u1: 1,
-            u2: 5,
-            u3: "0",
-            u4: "0",
-            q: "가사검색" + query,
-        }
+    try {
+        const response = await axios.get(base_url, {
+            params: {
+                where: "nexearch",
+                key: "LyricsSearchResult",
+                pkid: "519",
+                u1: 1,
+                u2: 5,
+                u3: "0",
+                u4: "0",
+                q: "가사검색" + query,
+            }
+        });
+        const currentHTML = response.data.current.html;
+        const currentResults = processLyricsInfo(currentHTML);
+
+        // Process the next HTML
+        const nextHTML = response.data.next.html;
+        const nextResults = processLyricsInfo(nextHTML);
+
+        // Combine the results if needed
+        const allResults = currentResults.concat(nextResults);
+
+        // Do something with the final results
+        console.log('Final Results:', allResults);
+    } catch (err) {
+        console.error('오류', err);
+    }
+};
+
+const processLyricsInfo = (html) => {
+    const $ = cheerio.load(html);
+
+    const results = [];
+
+    $('li[role="tab"]').each((index, element) => {
+        const title = $(element).find('.music_title a').text().trim();
+        const artist = $(element).find('.music_detail a').eq(0).text().trim();
+
+        results.push({
+            title,
+            artist,
+        });
     });
-    console.log(response.data)
+
+    return results;
 };
 
 searchLyric('연기처럼 밀려드는')
